380e1e934cf27c6ca731c68f48218b0a066de905
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2023-11-15 23:37:10 +0300

    Add files via upload

--- DFT_IDFT/DFT_IDFT_5.cpp
+++ DFT_IDFT/DFT_IDFT_5.cpp
@@ -5,39 +5,36 @@
 using namespace std;
 
 double u_f(double t, double x) {
-    return (t * t + 1) * sin(2 * M_PI * x);
+    //return (t * t + 1) * cos(x);
+    return (t + 1 ) * exp(sin(x));
 }
 
 double fi(double x) {
-    return sin(2 * M_PI * x);
-}
-
-double pi_1(double t) {
-    return 0;
-}
-
-double pi_2(double t) {
-    return (t * t + 1) * sin(2 * M_PI);
+    //return cos(2 * M_PI * x);
+    //return cos(x);
+    return exp(sin(x));
 }
 
 double f_(double t, double x) {
-    return 2 * t * sin(2 * M_PI * x) + 4 * M_PI * M_PI * (t * t + 1) * sin(2 * M_PI * x);
+    //return 2 * t * cos(2 * M_PI * x) + 4 * M_PI * M_PI * (t * t + 1) * cos(2 * M_PI * x);
+    //return 2 * t * cos(x) + (t * t + 1) * cos(x);
+    return exp(sin(x)) - (t + 1)*(exp(sin(x))* cos(x)*cos(x) -exp(sin(x))*sin(x));  
 }
 
 int main() {
-    double L = 1;
+    double L = 2 * M_PI;
     double T = 1;
-    int M = 64, N = 32;
-    cout << "Please input M , N : " << endl;
+    int M = 64, N = 1000;
+    //cout << "Please input M , N : " << endl;
 
     double h = L / M;
     double tau = T / N;
 
     double* u = (double*) fftw_malloc(sizeof(double) * (M));
     double* f = (double*) fftw_malloc(sizeof(double) * (M));
-    fftw_complex* u_k_n = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (M)/2+1);
-    fftw_complex* u_k_n1 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (M)/2+1);
-    fftw_complex* f_k = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * (M)/2+1);
+    fftw_complex* u_k_n = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ((M)/2+1));
+    fftw_complex* u_k_n1 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ((M)/2+1));
+    fftw_complex* f_k = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * ((M)/2+1));
     
     for (int i = 0; i < M; ++i) {
         u[i] = fi(i * h);
@@ -65,7 +62,7 @@ int main() {
             u_k_n1[k][0] = u_k_n[k][0] - (tau * alpha * alpha * k * k * u_k_n[k][0]) + tau * f_k[k][0];
             u_k_n1[k][1] = u_k_n[k][1] - (tau * alpha * alpha * k * k * u_k_n[k][1]) + tau * f_k[k][1];
         }
-        if (n == 1){
+        /*if (n == 2){
             cout << "u_k_n: "<< endl;
             for(int k = 0; k < M/2+1; k++){   
                 cout  << fixed << setprecision(30) <<u_k_n[k][0] <<" " << u_k_n[k][1] << endl;
@@ -76,29 +73,46 @@ int main() {
                 cout<< fixed << setprecision(30)  << f_k[k][0] <<" " << f_k[k][1] << endl;
             }
             cout << endl;
-            cout << "u_k_n1: "<< endl;
+            cout << "u_k_n+1: "<< endl;
             for(int k = 0; k < M/2+1; k++){   
                 cout << fixed << setprecision(30) << u_k_n1[k][0] <<" " << u_k_n1[k][1] << endl;
             }
             cout << endl;
-        }
+        }*/
         for (int k = 0; k < M/2+1; k++) {
             u_k_n[k][0] = u_k_n1[k][0];
             u_k_n[k][1] = u_k_n1[k][1];
         }
         fftw_execute(p3);
-        if (n == 1){
+        /*if (n == 200){
             for(int k = 0; k < M; k++){   
                 cout<< fixed << setprecision(30)  << u[k] <<" " << u_f(tau * n, k * h) << endl;
             }
-        }
+        }*/
     }
-    fftw_destroy_plan(p1);
-    fftw_destroy_plan(p2);
+    double error1, error2;
+    double sum = 0;
+    for(int i = 0; i < M; i++){
+            sum += pow((u_f(1, i * h) - u[i]), 2);
+    }
+    error1 = sqrt(sum * h);
+    double max = u_f(1, 0 * h) - u[0] ;
+    double value;
+    for(int i = 0; i < M; i++){
+        value = abs(u_f(1, i * h) - u[i]);
+        if (value > max)
+            max = value;    
+    }
+    cout << error1 << endl;
+    cout << max << endl;
+    
     fftw_destroy_plan(p3);
-    fftw_free(u_k_n);
-    fftw_free(u_k_n1);
+    fftw_destroy_plan(p2);
+    fftw_destroy_plan(p1);
+
     fftw_free(f_k);
+    fftw_free(u_k_n1);
+    fftw_free(u_k_n);
     fftw_free(f);
     fftw_free(u);
     return 0;
