42098f872fcb52b44449a3362a8e5a1fd09b313e
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2024-03-14 23:47:55 +0300

    Update r2r_r2c2.cpp

--- DST_IDST3d/r2r_r2c2.cpp
+++ DST_IDST3d/r2r_r2c2.cpp
@@ -11,9 +11,23 @@ double func(double x, double y, double z)
 	return sin(x)*cos(y)*cos(2*M_PI*z);
 }
 
-double dfunc(double x, double y, double z)
+double dfunc_xy(double x, double y, double z)
 {
     return  -func(x,y,z);   
+}
+
+double dfunc_z(double x, double y, double z)
+{
+    return  -4*M_PI*M_PI*func(x,y,z);   
+}
+
+double err_calculation(const double* n1, const  double* n2, int size){
+    double err = 0.0;
+    for (int i = 0; i < size; ++i) {
+        double err_ = fabs(n1[i] - n2[i]);
+        err += err_ * err_;
+    }
+    return  sqrt(err);
 }
 
 int main() {
@@ -28,6 +42,9 @@ int main() {
     double L_x = 2*M_PI, L_y = 2*M_PI, L_z = 1;
     double *in_ = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *in = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
+    double *in2 = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
+    double *in_dfunc_xy = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
+    double *in_dfunc_z = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *ino = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *out_z = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     fftw_complex *out = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
@@ -37,6 +54,8 @@ int main() {
             for(int k = 0; k < n3; ++k) {
                 in[(i*N2+j)*n3+k] = func(i*L_x/N1, j*L_y/N2, k*L_z/N3);
                 in_[(i*N2+j)*n3+k] = func(i*L_x/N1, j*L_y/N2, k*L_z/N3);
+                in_dfunc_xy[(i*N2+j)*n3+k] = dfunc_xy(i*L_x/N1, j*L_y/N2, k*L_z/N3);
+                in_dfunc_z[(i*N2+j)*n3+k] = dfunc_z(i*L_x/N1, j*L_y/N2, k*L_z/N3);
             }
         }
     }
@@ -57,14 +76,6 @@ int main() {
 
     fftw_execute(plan_r2r_z);
     fftw_destroy_plan(plan_r2r_z);
-
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < N2; ++j) {
-            for(int k = 0; k < n3; ++k) {
-                cout << i << " " << j << " " << k << " " << ino[(i*N2+j)*n3+k]<< endl;
-            }
-        }
-    }
 
     // Forward transformation R2 -> C2 x,y
     int nn[] = {N1, N2};
@@ -87,14 +98,6 @@ int main() {
         out[i][0] /= N1*N2;
         out[i][1] /= N1*N2;
     }
-
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < (N2/2+1); ++j) {
-            for(int k = 0; k < n3; ++k) {
-                //cout << i << " " << j << " " << k << " " << out[(i * N2 + j) * n3 + k][0] << " " << out[(i * N2 + j) * n3 + k][1] << endl;
-            }
-        }
-    }
     
     // Backward transformation C2 -> R2 x,y
     inembed2[0] = N1; inembed2[1] = N2/2+1;
@@ -112,6 +115,7 @@ int main() {
     for (int i = 0; i < N1*N2*n3; ++i)
     {
         in[i] /= N3;
+        in2[i] = in[i]; // save value in[]
     }
 
     // Backward transformation R -> R z
@@ -125,25 +129,60 @@ int main() {
     fftw_plan plan_r2r_z_inverse = fftw_plan_many_r2r(rank, n, howmany,
                                                       in, inembed3, istride, idist,
                                                       out_z, onembed3, ostride, odist,
-                                                      kind, FFTW_ESTIMATE);
+                                                      kind, FFTW_ESTIMATE);    
+    // calculate d2f/dx2
+    for(int i = 0; i < N1; ++i) {
+        for(int j = 0; j < N2; ++j) {
+            for(int k = 0; k < n3; ++k) {
+                in[(i*N2+j)*n3+k] *= -k*k; // Здесь я не уверен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //Не знаю почему: если я сделал так double k_x = i <= nx/2 ? i : i - nx  результат неправильный
+            }
+        }
+    }                                              
     fftw_execute(plan_r2r_z_inverse);
-    fftw_destroy_plan(plan_r2r_z_inverse);
+    cout << "err d2f/dx2 = " << err_calculation(out_z, in_dfunc_xy, N1*N2*n3) << endl;
 
-
-    double err = 0.0;
+    // calculate d2f/dy2
     for (int i = 0; i < N1*N2*n3; ++i)
     {
-        double err_ = fabs(out_z[i] - in_[i]);
-        err += err_ * err_;
-        //cout << out_z[i] << " " << in_[i]<< endl;
+        in[i] = in2[i];
     }
-    
-    err = sqrt(err);
-    
-    cout << "err = " << err << endl;
+    for(int i = 0; i < N1; ++i) {
+        for(int j = 0; j < N2; ++j) {
+            for(int k = 0; k < n3; ++k) {
+                in[(i*N2+j)*n3+k] *= -k*k;// Здесь я не уверен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                //Не знаю почему: если я сделал так double k_y = i <= nx/2 ? i : i - ny  результат неправильный
+            }
+        }
+    }                                              
+    fftw_execute(plan_r2r_z_inverse);
+    cout << "err d2f/dy2 = " << err_calculation(out_z, in_dfunc_xy, N1*N2*n3) << endl;
+
+    // calculate d2f/dz2
+    for (int i = 0; i < N1*N2*n3; ++i)
+    {
+        in[i] = in2[i];
+    }
+    double alpha = 2*M_PI/L_z;
+    for(int i = 0; i < N1; ++i) {
+        for(int j = 0; j < N2; ++j) {
+            for(int k = 0; k < n3; ++k) {
+                in[(i*N2+j)*n3+k] *= -k*k*alpha*alpha;
+            }
+        }
+    }   
+    fftw_execute(plan_r2r_z_inverse);
+    cout << "err d2f/dz2 = " << err_calculation(out_z, in_dfunc_z, N1*N2*n3) << endl;
+
+    fftw_destroy_plan(plan_r2r_z_inverse);
+
     free(in);
     free(ino);
     free(out_z);
     free(out);
+    free(in_);
+    free(in2);
+    free(in_dfunc_xy);
+    free(in_dfunc_z);
     return 0;
 }
