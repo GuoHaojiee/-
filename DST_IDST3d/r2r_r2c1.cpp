b0950fac6d183e242cf88596313647c6c23ac604
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2024-03-20 10:13:57 +0300

    Update r2r_r2c1.cpp

--- DST_IDST3d/r2r_r2c1.cpp
+++ DST_IDST3d/r2r_r2c1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 double func(double x, double y, double z)
 {
-	return sin(x)*cos(y)*cos(2*M_PI*z);
+	return sin(3*x)* cos(5*y)*cos(2 *M_PI*z);
 }
 
 double dfunc(double x, double y, double z)
@@ -29,7 +29,6 @@ int main() {
     double *in_ = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *in = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *ino = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
-    double *ino_ = (double*)fftw_malloc(sizeof(double) * N1 * N2 * N3);
     double *out_z = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *out_xy = (double*)fftw_malloc(sizeof(double) * N1 * N2 * N3);
     
@@ -61,37 +60,28 @@ int main() {
 
     fftw_execute(plan_r2r_z);
     fftw_destroy_plan(plan_r2r_z);
-    
+
     for(int i = 0; i < N1; ++i) {
         for(int j = 0; j < N2; ++j) {
             for(int k = 0; k < n3; ++k) {
-                //cout << i << " " << j << " " << k << " " << ino[(i*N2+j)*n3+k]<< endl;
+                int index = (i * N2 + j)*n3 + k;
+                double mod2 = ino[index]*ino[index];
+                if (mod2 > 1e-5)
+                    cout << "out r2r " << i << " " << j << " " << k << " " << ino[index]<< endl;
             }
         }
     }
-    
-    //For the case of REDFT00, this even symmetry means that X_j = X_N-j
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < N2; ++j) {
-            ino_[(i*N2+j)*N3] = ino[(i*N2+j)*n3];
-            for(int k = 1; k < n3; ++k) {
-                ino_[(i*N2+j)*N3+k] = ino[(i*N2+j)*n3+k];
-                ino_[(i*N2+j)*N3+N3-k] = ino[(i*N2+j)*n3+k];
-            }
-        }
-    }
-    
-
+    cout << endl;
     // Forward transformation R2 -> C2 x,y
     int nn[] = {N1, N2};
     int inembed2[] =  {N1, N2};
     int onembed2[] =  {N1, N2/2+1};
-    istride = 1; ostride = 1;
-    idist = N1*N2; odist = N1*(N2/2+1);
+    istride = n3; ostride = n3;
+    idist = 1; odist = 1;
     
-    fftw_complex *out = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*N3 * sizeof(fftw_complex));
-    fftw_plan fplan_3d = fftw_plan_many_dft_r2c(2, nn, N3,
-                                                ino_, inembed2, istride, idist,
+    fftw_complex *out = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_plan fplan_3d = fftw_plan_many_dft_r2c(2, nn, n3,
+                                                ino, inembed2, istride, idist,
                                                 out, onembed2, ostride, odist,
                                                 FFTW_ESTIMATE);
     fftw_execute(fplan_3d);
@@ -99,7 +89,7 @@ int main() {
     
 
     //Normalization
-    for (int i = 0; i < N1*(N2/2+1)*N3; ++i)
+    for (int i = 0; i < N1*(N2/2+1)*n3; ++i)
     {
         out[i][0] /= N1*N2;
         out[i][1] /= N1*N2;
@@ -108,7 +98,10 @@ int main() {
     for(int i = 0; i < N1; ++i) {
         for(int j = 0; j < (N2/2+1); ++j) {
             for(int k = 0; k < n3; ++k) {
-                cout << i << " " << j << " " << k << " " << out[(i * N2 + j) * n3 + k][0] << " " << out[(i * N2 + j) * n3 + k][1] << endl;
+                int index = (i * (N2/2+1) + j) * n3 + k;
+                double mod2 = out[index][0]*out[index][0] + out[index][1]*out[index][1];
+                if (mod2 > 1e-10)
+                    cout << "out r2c " << i << " " << j << " " << k << " " << out[index][0] << " " << out[index][1] << endl;
             }
         }
     }
@@ -116,11 +109,11 @@ int main() {
     // Backward transformation C2 -> R2 x,y
     inembed2[0] = N1; inembed2[1] = N2/2+1;
     onembed2[0] = N1; onembed2[1] = N2;
-    istride = 1; ostride = 1;
-    idist = N1*(N2/2+1); odist = N1*N2;
-    fftw_plan bplan_3d = fftw_plan_many_dft_c2r(2, nn, N3,
+    istride = n3; ostride = n3;
+    idist = 1; odist = 1;
+    fftw_plan bplan_3d = fftw_plan_many_dft_c2r(2, nn, n3,
                                                     out, inembed2, istride, idist,
-                                                    out_xy, onembed2, ostride, odist,
+                                                    ino, onembed2, ostride, odist,
                                                     FFTW_ESTIMATE);
     fftw_execute(bplan_3d);
     fftw_destroy_plan(bplan_3d);
@@ -129,8 +122,7 @@ int main() {
     for(int i = 0; i < N1; ++i) {
         for(int j = 0; j < N2; ++j) {
             for(int k = 0; k < n3; ++k) {
-                out_xy[(i*N2+j)*N3+k] /= N3;
-                in[(i*N2+j)*n3+k] = out_xy[(i*N2+j)*N3+k];
+                ino[(i*N2+j)*n3+k] /= N3;
             }
         }
     }
@@ -144,7 +136,7 @@ int main() {
     int *inembed3 = n, *onembed3 = n;
     fftw_r2r_kind kind_z_inverse[] = {FFTW_REDFT00};
     fftw_plan plan_r2r_z_inverse = fftw_plan_many_r2r(rank, n, howmany,
-                                                      in, inembed3, istride, idist,
+                                                      ino, inembed3, istride, idist,
                                                       out_z, onembed3, ostride, odist,
                                                       kind, FFTW_ESTIMATE);
     fftw_execute(plan_r2r_z_inverse);
@@ -164,7 +156,6 @@ int main() {
     cout << "err = " << err << endl;
     free(in);
     free(ino);
-    free(ino_);
     free(out_z);
     free(out_xy);
     free(out);
