555859ae52b61bc1094feea5d43f79fbd5ffa023
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2024-03-21 00:03:13 +0300

    Update r2r_r2c2.cpp

--- DST_IDST3d/r2r_r2c2.cpp
+++ DST_IDST3d/r2r_r2c2.cpp
@@ -40,22 +40,20 @@ int main() {
     int howmany;
     int rank;
     double L_x = 2*M_PI, L_y = 2*M_PI, L_z = 1;
-    double *in_ = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *in = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
-    double *in2 = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
+    double *ino = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *in_dfunc_xy = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     double *in_dfunc_z = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
-    double *ino = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
-    double *out_z = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
+    double *d_out = (double*)fftw_malloc(sizeof(double) * N1 * N2 * n3);
     fftw_complex *out = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
 
     for(int i = 0; i < N1; ++i) {
         for(int j = 0; j < N2; ++j) {
             for(int k = 0; k < n3; ++k) {
-                in[(i*N2+j)*n3+k] = func(i*L_x/N1, j*L_y/N2, k*L_z/N3);
-                in_[(i*N2+j)*n3+k] = func(i*L_x/N1, j*L_y/N2, k*L_z/N3);
-                in_dfunc_xy[(i*N2+j)*n3+k] = dfunc_xy(i*L_x/N1, j*L_y/N2, k*L_z/N3);
-                in_dfunc_z[(i*N2+j)*n3+k] = dfunc_z(i*L_x/N1, j*L_y/N2, k*L_z/N3);
+                int index = (i*N2+j)*n3+k;
+                in[index] = func(i*L_x/N1, j*L_y/N2, k*L_z/N3);
+                in_dfunc_xy[index] = dfunc_xy(i*L_x/N1, j*L_y/N2, k*L_z/N3);
+                in_dfunc_z[index] = dfunc_z(i*L_x/N1, j*L_y/N2, k*L_z/N3);
             }
         }
     }
@@ -73,7 +71,6 @@ int main() {
                                               in, inembed, istride, idist,
                                               ino, onembed, ostride, odist,
                                               kind, FFTW_ESTIMATE);
-
     fftw_execute(plan_r2r_z);
     fftw_destroy_plan(plan_r2r_z);
 
@@ -81,8 +78,8 @@ int main() {
     int nn[] = {N1, N2};
     int inembed2[] =  {N1, N2};
     int onembed2[] =  {N1, N2/2+1};
-    istride = 1; ostride = 1;
-    idist = N1*N2; odist = N1*(N2/2+1);
+    istride = n3; ostride = n3;
+    idist = 1; odist = 1;
 
     fftw_plan fplan_3d = fftw_plan_many_dft_r2c(2, nn, n3,
                                                 ino, inembed2, istride, idist,
@@ -92,32 +89,42 @@ int main() {
     fftw_execute(fplan_3d);
     fftw_destroy_plan(fplan_3d);
 
+    fftw_complex *out_x = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_complex *out_y = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_complex *out_z = (fftw_complex*)fftw_malloc(N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    
     //Normalization
-    for (int i = 0; i < N1*(N2/2+1)*n3; ++i)
-    {
-        out[i][0] /= N1*N2;
-        out[i][1] /= N1*N2;
-    }
+    for(int i = 0; i < N1; ++i) {
+        for(int j = 0; j < (N2/2+1); ++j) {
+            for(int k = 0; k < n3; ++k) {
+                int index = (i*(N2/2+1)+j)*n3+k;
+                int k_x = i <= N1/2 ? i : i - N1;
+                int k_y = j <= N2/2 ? j : j - N2;
+                out[index][0] /= N1*N2*N3;
+                out[index][1] /= N1*N2*N3;
+                out_x[index][0] = out[index][0] * -(k_x)*(k_x); 
+                out_x[index][1] = out[index][1] * -(k_x)*(k_x);
+                
+                out_y[index][0] = out[index][0] * -(k_y)*(k_y); 
+                out_y[index][1] = out[index][1] * -(k_y)*(k_y);
+
+                double alpha = 2*M_PI/L_z;
+                out_z[index][0] = out[index][0] * -k*k*alpha*alpha;
+                out_z[index][1] = out[index][1] * -k*k*alpha*alpha;
+            }
+        }
+    }  
     
     // Backward transformation C2 -> R2 x,y
     inembed2[0] = N1; inembed2[1] = N2/2+1;
     onembed2[0] = N1; onembed2[1] = N2;
-    istride = 1; ostride = 1;
-    idist = N1*(N2/2+1); odist = N1*N2;
+    istride = n3; ostride = n3;
+    idist = 1; odist = 1;
     fftw_plan bplan_3d = fftw_plan_many_dft_c2r(2, nn, n3,
                                                     out, inembed2, istride, idist,
-                                                    in, onembed2, ostride, odist,
+                                                    ino, onembed2, ostride, odist,
                                                     FFTW_ESTIMATE);
-    fftw_execute(bplan_3d);
-    fftw_destroy_plan(bplan_3d);
-    
-    //Normalization
-    for (int i = 0; i < N1*N2*n3; ++i)
-    {
-        in[i] /= N3;
-        in2[i] = in[i]; // save value in[]
-    }
-
+        
     // Backward transformation R -> R z
     rank = 1;
     n[0] = n3;
@@ -127,62 +134,38 @@ int main() {
     int *inembed3 = n, *onembed3 = n;
     fftw_r2r_kind kind_z_inverse[] = {FFTW_REDFT00};
     fftw_plan plan_r2r_z_inverse = fftw_plan_many_r2r(rank, n, howmany,
-                                                      in, inembed3, istride, idist,
-                                                      out_z, onembed3, ostride, odist,
-                                                      kind, FFTW_ESTIMATE);    
+                                                      ino, inembed3, istride, idist,
+                                                      d_out, onembed3, ostride, odist,
+                                                      kind, FFTW_ESTIMATE);
+                                                      
     // calculate d2f/dx2
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < N2; ++j) {
-            for(int k = 0; k < n3; ++k) {
-                in[(i*N2+j)*n3+k] *= -k*k; // Здесь я не уверен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                //Не знаю почему: если я сделал так double k_x = i <= nx/2 ? i : i - nx  результат неправильный
-            }
-        }
-    }                                              
+    memcpy(out, out_x, N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_execute(bplan_3d);
     fftw_execute(plan_r2r_z_inverse);
-    cout << "err d2f/dx2 = " << err_calculation(out_z, in_dfunc_xy, N1*N2*n3) << endl;
+    cout << "err d2f/dx2 = " << err_calculation(d_out, in_dfunc_xy, N1*N2*n3) << endl;
 
     // calculate d2f/dy2
-    for (int i = 0; i < N1*N2*n3; ++i)
-    {
-        in[i] = in2[i];
-    }
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < N2; ++j) {
-            for(int k = 0; k < n3; ++k) {
-                in[(i*N2+j)*n3+k] *= -k*k;// Здесь я не уверен!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                //Не знаю почему: если я сделал так double k_y = i <= nx/2 ? i : i - ny  результат неправильный
-            }
-        }
-    }                                              
+    memcpy(out, out_y, N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_execute(bplan_3d);
     fftw_execute(plan_r2r_z_inverse);
-    cout << "err d2f/dy2 = " << err_calculation(out_z, in_dfunc_xy, N1*N2*n3) << endl;
+    cout << "err d2f/dy2 = " << err_calculation(d_out, in_dfunc_xy, N1*N2*n3) << endl;
 
     // calculate d2f/dz2
-    for (int i = 0; i < N1*N2*n3; ++i)
-    {
-        in[i] = in2[i];
-    }
-    double alpha = 2*M_PI/L_z;
-    for(int i = 0; i < N1; ++i) {
-        for(int j = 0; j < N2; ++j) {
-            for(int k = 0; k < n3; ++k) {
-                in[(i*N2+j)*n3+k] *= -k*k*alpha*alpha;
-            }
-        }
-    }   
+    memcpy(out, out_z, N1*(N2/2+1)*n3 * sizeof(fftw_complex));
+    fftw_execute(bplan_3d);
     fftw_execute(plan_r2r_z_inverse);
-    cout << "err d2f/dz2 = " << err_calculation(out_z, in_dfunc_z, N1*N2*n3) << endl;
-
-    fftw_destroy_plan(plan_r2r_z_inverse);
+    cout << "err d2f/dz2 = " << err_calculation(d_out, in_dfunc_z, N1*N2*n3) << endl;
 
     free(in);
     free(ino);
-    free(out_z);
-    free(out);
-    free(in_);
-    free(in2);
     free(in_dfunc_xy);
     free(in_dfunc_z);
+    free(out);
+    free(out_x);
+    free(out_y);
+    free(out_z);
+    free(d_out);
+    fftw_destroy_plan(plan_r2r_z_inverse);  
+    fftw_destroy_plan(bplan_3d); 
     return 0;
 }
